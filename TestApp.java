62774752b25eec2780648158e2ac6026ed10cd41
sajid hussain <hsajid09@gmail.com>
2019-08-13 21:40:52 -0700

    fist TestApp.java commit

---
+++ TestApp.java
@@ -0,0 +1,30 @@
+package com.java.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		
+		Configuration con=new Configuration().configure();
+		SessionFactory sf=con.buildSessionFactory();
+		Session session=sf.openSession();
+		Transaction tx=session.beginTransaction();
+		Alien alien=new Alien();
+		
+		/*alien.setAname("Scott");
+		alien.setColor("Greeen");
+		session.save(alien);
+		tx.commit();
+		session.close();*/
+		session=sf.openSession();
+		alien=session.get(Alien.class, 1);
+		System.out.println(alien);
+		session.close();
+
+	}
+
+}
